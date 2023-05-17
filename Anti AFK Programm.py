74fd62263f1dc51dc71514e81c6634886d7bec52
Eragon1250 <133884088+Eragon1250@users.noreply.github.com>
2023-05-17 16:55:43 +0200

    Add files via upload

---
+++ Anti AFK Programm.py
@@ -0,0 +1,38 @@
+import keyboard as key
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
+import time
+import random
+
+while True:
+	time.sleep(0.001)
+	if key.is_pressed('*'):
+		time.sleep(0.5)
+		while not key.is_pressed('*'):
+			keyboard.press('w')
+			time.sleep(random.randint(1, 2))
+			keyboard.release('w')
+			keyboard.press('s')
+			time.sleep(random.randint(1, 2))
+			keyboard.release('s')
+			keyboard.press('a')
+			time.sleep(random.randint(1, 2))
+			keyboard.release('a')
+			keyboard.press('d')
+			time.sleep(random.randint(1, 2))
+			keyboard.release('d')
+			keyboard.press('e')
+			time.sleep(random.randint(0, 1))
+			keyboard.release('e')
+		else:
+			keyboard.release('w')
+			keyboard.release('a')
+			keyboard.release('s')
+			keyboard.release('d')
+		while key.is_pressed('*'):
+			None
+
+			
+
+
+
